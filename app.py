38ddcb3bd7fbe67a186c279909ac893cc49b4584
Kunal Bhat <kunalbhat@gmail.com>
2014-05-31 10:18:02 -0500

    Initial set of words

---
+++ app.py
@@ -0,0 +1,15 @@
+import random
+
+words = ['morel', 'crimini', 'portabella', 'maitake', 'shiitake', 'enoki',
+         'oyster', 'beech', 'chanterelle', 'boletus edulis', 'cantharellus cibarius',
+         'cantharellus tubaeformis', 'clitocybe nuda', 'cortinarius caperatus',
+         'craterellus cornucopioides', 'grifola frondosa', 'gyromitra esculenta',
+         'hericium erinaceus', 'hydnum repandum', 'lactarius deliciosus', 'morchella',
+         'tricholoma matsutake', 'amanita caesarea', 'armillaria mellea', 'boletus badius',
+         'chroogomphus rutilus', 'calvatia gigantea', 'calocybe gambosa', 'clavariaceae',
+         'clavulinaceae', 'coprinus comatus', 'fistulina hepatica', 'cortinarius variicolor',
+         'hygrophorus chrysodon', 'lactarius salmonicolor', 'lactarius volemus', 'lactarius subdulcis',
+         'laetiporus sulphureus', 'leccinum aurantiacum', 'leccinum scabrum', 'lepiota procera'
+        ]
+
+print words
